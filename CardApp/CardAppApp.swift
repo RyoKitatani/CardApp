ae7faddf1f245e165fb767810824486a67b9a337
RyoKitatani <ryo0929m@gmail.com>
2021-05-13 18:21:24 +0900

    Initial Commit

---
+++ CardApp/CardAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardAppApp.swift
+//  CardApp
+//
+//  Created by RyoKitatani on 2021/05/13.
+//
+
+import SwiftUI
+
+@main
+struct CardAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
